8dd65005af9fd2611273b32172361109e3077d91
devendrarawat-code <devendrarawat9876@gmail.com>
2026-06-27 10:00:00 +0530

    updated the data of the projects data

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    id: 1,
+    title: "CodeStash",
+    description:
+      " A clean, full-stack code snippet manager built with React, Vite, and Firebase — save, tag, search, and instantly copy your favorite snippets with real-time sync.",
+    image: "src/assets/codestash.png",
+    tech: ["React", "Tailwind CSS", "Vite"],
+    live: "https://code-stash-six.vercel.app/",
+    github: "https://github.com/bytesbydev/CodeStash",
+  },
+  {
+    id: 2,
+    title: "Cerso Landing Page",
+    description:
+      "A premium SaaS-inspired landing page developed during my Web Development Internship at Cerso.",
+    image:"src/assets/cerso.png",
+    tech: ["React", "Tailwind CSS", "Vite"],
+    live: "https://cerso-landing-page-kappa.vercel.app/",
+    github: "https://github.com/bytesbydev/CERSO-Landing-Page",
+  },
+  {
+    id: 3,
+    title: "ScanStack",
+    description:
+      "👾 ScanStack is a smart web application that analyzes resumes using AI and provides detailed ATS optimization insights. It evaluates your resume structure, skills, and content to generate actionable feedback for improving job readiness and recruiter visibility.",
+    image: "src/assets/scanstack.png",
+    tech: ["React", "Google Gemini API", " Lucide Icons","React Router DOM"],
+    live: "https://ai-resume-analyzer-seven-rouge.vercel.app/",
+    github: "https://github.com/bytesbydev/ScanStack",
+  },
+  {
+    id: 4,
+    title: "Recipe-finder",
+    description:
+      "About Recipe Finder is a web app that lets you discover recipes by simply entering an ingredient. It displays recipe images, a full list of ingredients, step-by-step instructions, and links to YouTube videos for tutorials. Perfect for finding quick, tasty meals with what you have on hand!.",
+    image: "src/assets/recieoe.png",
+    tech: ["React", "Firebase", "Tailwind"],
+    live: "https://recipe-finder-neon-two.vercel.app",
+    github: "https://github.com/bytesbydev/Recipe-Finder",
+  },
+];
